25366040ad11824733b75986af896a5e43b96694
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-02-11 15:41:43 +0530

    [Deep]Update: file update for checking options

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -2,10 +2,33 @@
 public class SnakeAndLadder {
 
     public static void main(String[] args) {
-        int player1;
-        int position;
-        double dice = (Math.floor(Math.random() * 10) % 6)+1;
 
-        System.out.println("dice number : " + dice);
+        int position = 0;
+        final int snake = 2;
+        final int ladder = 1;
+
+        double option = Math.floor(Math.random() * 10) % 3;
+        double diceNo = (Math.floor(Math.random() * 10) % 6)+1;
+
+
+        switch((int)option){
+            case ladder:
+                position = position+(int)diceNo;
+                System.out.println("player Got The Ladder and move to: "+position);
+                break;
+
+            case snake:
+                if (position==0){
+                    System.out.println("player at ZERO TRY AGAIN");
+                    break;
+                }
+                else {
+                    position = position - (int) diceNo;
+                    System.out.println("Player Got The Snake And Move To: "+ position);
+                }
+                break;
+            default:
+                System.out.println("Player is Not Playing Stay at Same Position: "+position);
+        }
     }
 }
