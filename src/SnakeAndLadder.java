16af9b06d1cd2ab0a81573fe9b6f0db7f411af54
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-02-11 17:33:34 +0530

    [Deep]Updated: file Updated to ensure player gets exact 100

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -8,34 +8,46 @@ public class SnakeAndLadder {
         final int ladder = 1;
 
         while (position<=100) {
+
             double option = Math.floor(Math.random() * 10) % 3;
             double diceNo = (Math.floor(Math.random() * 10) % 6) + 1;
 
+                if ((int)option==ladder) {
 
-            switch ((int) option) {
-                case ladder:
-                    position = position + (int)diceNo;
-                    System.out.println("player Got The Ladder with "+(int)diceNo+" and move to: " + position);
-                    break;
-
-                case snake:
-                    if (position >= 0) {
-                        position = position - (int) diceNo;
-                        System.out.println("Player Got The Snake with "+(int)diceNo+" And Move To: " + position);
-
-                    } else {
-                        System.out.println("player at ZERO TRY AGAIN");
-                        position=0;
+                    if (position + (int) diceNo == 100) {
+                        final int end = position+(int)diceNo;
+                        System.out.println("Player Got The Ladder with " + (int)diceNo + " and move to: " + end);
+                        System.out.println("**HURRREYY U WIN THE GAME**");
+                        break;
                     }
 
-                    break;
-                default:
+                    else if (position + (int)diceNo > 100) {
+                        position = position - (int)diceNo;
+                    }
+
+                    else if (position + (int)diceNo < 100) {
+                        position = position + (int)diceNo;
+                        System.out.println("player Got The Ladder with " + (int)diceNo + " and move to: " + position);
+                    }
+                }
+
+                else if ((int)option==snake) {
+
+                    if (position >= 0) {
+                        position = position - (int)diceNo;
+                        System.out.println("Player Got The Snake with " + (int)diceNo + " And Move To: " + position);
+                    }
+
+                    else {
+                        System.out.println("player at ZERO TRY AGAIN");
+                        position = 0;
+                    }
+                }
+
+                else {
                     System.out.println("Player is Not Playing Stay at Same Position: " + position);
-
-
-            }
+                }
 
         }
-
     }
 }
