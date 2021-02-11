c73e2503ed1740d00c402de7c03659f7a5bbb5c9
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-02-11 19:16:04 +0530

    [Deep]Updated: file updated for 2 players SnakeAndLadder Game

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,57 +1,90 @@
-
 public class SnakeAndLadder {
 
     public static void main(String[] args) {
         final int snake = 2;
         final int ladder = 1;
-        int count = 0;
+
+        int count1 = 0;
+        int count2 = 0;
         int position = 0;
+        int position1 = 0;
 
 
-        while (position<=100) {
+        outer: while (true) {
 
-            double option = Math.floor(Math.random() * 10) % 3;
-            double diceNo = (Math.floor(Math.random() * 10) % 6) + 1;
+            while (position <= 100) {
 
-                if ((int)option==ladder) {
+                double option = Math.floor(Math.random() * 10) % 3;
+                double diceNo = (Math.floor(Math.random() * 10) % 6) + 1;
+
+                if ((int) option == ladder) {
 
                     if (position + (int) diceNo == 100) {
-                        final int end = position+(int)diceNo;
-                        System.out.println("Player Got The Ladder with " + (int)diceNo + " and move to: " + end);
-                        System.out.println("**HURRREYY U WIN THE GAME**");
-                        break;
+                        final int end = position + (int) diceNo;
+                        System.out.println("Player1 Got The Ladder with " + (int) diceNo + " and move to: " + end);
+                        System.out.println("**HURRREYY PLAYER1 WIN THE GAME**");
+                        break outer;
+                    } else if (position + (int) diceNo > 100) {
+                        position = position - (int) diceNo;
+                    } else if (position + (int) diceNo < 100) {
+                        position = position + (int) diceNo;
+                        System.out.println("player1 Got The Ladder with " + (int) diceNo + " and move to: " + position);
                     }
-
-                    else if (position + (int)diceNo > 100) {
-                        position = position - (int)diceNo;
-                    }
-
-                    else if (position + (int)diceNo < 100) {
-                        position = position + (int)diceNo;
-                        System.out.println("player Got The Ladder with " + (int)diceNo + " and move to: " + position);
-                    }
-                }
-
-                else if ((int)option==snake) {
+                } else if ((int) option == snake) {
 
                     if (position >= 0) {
-                        position = position - (int)diceNo;
-                        System.out.println("Player Got The Snake with " + (int)diceNo + " And Move To: " + position);
-                    }
-
-                    else {
-                        System.out.println("player at ZERO TRY AGAIN");
+                        position = position - (int) diceNo;
+                        System.out.println("Player1 Got The Snake with " + (int) diceNo + " And Move To: " + position);
+                    } else {
+                        System.out.println("player1 at ZERO TRY AGAIN");
                         position = 0;
                     }
+                    break;
+                } else {
+                    System.out.println("Player1 is Not Playing Stay at Same Position: " + position);
+                    break;
                 }
+                System.out.println("Player1 Current Position Is " + position);
+                count1++;
+            }
+            while (position1 <= 100) {
 
-                else {
-                    System.out.println("Player is Not Playing Stay at Same Position: " + position);
+                double option = Math.floor(Math.random() * 10) % 3;
+                double diceNo = (Math.floor(Math.random() * 10) % 6) + 1;
+
+                if ((int) option == ladder) {
+
+                    if (position1 + (int) diceNo == 100) {
+                        final int end1 = position + (int) diceNo;
+                        System.out.println("Player2 Got The Ladder with " + (int) diceNo + " and move to: " + end1);
+                        System.out.println("**HURRREYY PLAYER2 WIN THE GAME**");
+                        break outer;
+                    } else if (position1 + (int) diceNo > 100) {
+                        position = position1 - (int) diceNo;
+                    } else if (position1 + (int) diceNo < 100) {
+                        position1 = position1 + (int) diceNo;
+                        System.out.println("player2 Got The Ladder with " + (int) diceNo + " and move to: " + position1);
+                    }
+                } else if ((int) option == snake) {
+
+                    if (position1 >= 0) {
+                        position1 = position1 - (int) diceNo;
+                        System.out.println("Player2 Got The Snake with " + (int) diceNo + " And Move To: " + position1);
+                    } else {
+                        System.out.println("player2 at ZERO TRY AGAIN");
+                        position1 = 0;
+                    }
+                    break;
+                } else {
+                    System.out.println("Player2 is Not Playing Stay at Same Position: " + position1);
+                    break;
                 }
-            System.out.println("Player Current Position Is " +position );
-            count++;
+                System.out.println("Player2 Current Position Is " + position1);
+                count2++;
+            }
         }
-        System.out.println("Number Of Times The Dice Was Roll: "+count);
-    }
+            System.out.println("Number Of Times The Dice Was Rolled By Player1: " + count1);
+            System.out.println("Number Of Times The Dice Was Rolled By Player2: " + count2);
 
+    }
 }
